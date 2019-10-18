abad7dd652269c205240fcc79e1c8ec7317afa19
lumuy <dxgnaw@gmail.com>
2019-10-18 17:33:34 +0800

    Complete day 13

--- day13/handle.rb
+++ day13/handle.rb
@@ -1,221 +1,119 @@
-require 'pry'
-
 class Handle
+  attr_accessor :cars, :map, :directions, :intersections, :intersection_counts
+
   def initialize(file = 'data')
-    @data, @types = {}, %w[< > ^ v]
+    @map, @directions = {}, %w[^ v > <]
     File.read(file).split("\n").each_with_index do |str, x|
-      str.chars.each_with_index { |char, y| @data.merge!([y, x] => char) unless char == ' ' }
+      str.chars.each_with_index { |char, y| @map.merge!([y, x] => char) unless char == ' ' }
+    end
+    @cars = @map.select { |_, v| @directions.include?(v) }
+    @intersections = @map.select { |_, v| v == '+' }.keys
+    @intersection_counts = @cars.reduce({}) { |res, (p, _)| res.merge(p => 0) }
+
+    @cars.each do |point, direction|
+      @map[point] = '-' if ['>', '<'].include?(direction)
+      @map[point] = '|' if ['^', 'v'].include?(direction)
     end
   end
 
-  # Part 1
-  def location_of_first_crash(area = @data.dup, types = @types, origin = @data.dup)
-    cars = area.select { |_, v| types.include?(v) }.to_a
-    intersection_points  = area.select { |_, v| v == '+' }.keys
-    intersection_counts  = Array.new(cars.size, 0)
-    next_directions      = [:left, :straight, :right]
-    points               = area.keys
-    status               = true
+  def run
+    echo_first_crash = true
 
-    loop do
-      temp_area = area.dup
-      new_cars = []
-      crash = []
-      next_positions = step_position(cars, area.dup)
+    while @cars.count > 1
+      cars, skip_points = {}, []
+      points = @cars.keys
 
-      0.upto(cars.size - 1).each do |index|
-        next_position = next_positions[index]
-        current_position, current_direction = cars[index]
-        next_direction = nil
+      @cars.dup.each do |point, direction|
+        next if skip_points.include?(point)
 
-        state = area[next_position]
-        state = origin[next_position] if types.include?(state)
-        case state
-        when '|'
-          next_direction = current_direction if ['^', 'v'].include?(current_direction)
-        when '-'
-          next_direction = current_direction if ['<', '>'].include?(current_direction)
-        when '/'
-          next_direction = 'v' if current_direction == '<'
-          next_direction = '>' if current_direction == '^'
-          next_direction = '^' if current_direction == '>'
-          next_direction = '<' if current_direction == 'v'
-        when "\\"
-          next_direction = 'v' if current_direction == '>'
-          next_direction = '<' if current_direction == '^'
-          next_direction = '>' if current_direction == 'v'
-          next_direction = '^' if current_direction == '<'
-        when '+'
-          next_direction = next_intersection_direction(
-            current_direction,
-            next_directions[intersection_counts[index] % 3]
-          )
+        new_point = car_next_point(point, direction)
 
-          intersection_counts[index] += 1
-        end
+        # crash
+        if points.include?(new_point)
+          # Part 1
+          puts new_point.join(',') if echo_first_crash
+          echo_first_crash = false
 
-        if intersection_points.include?(current_position)
-          temp_area[current_position] = '+'
-        else
-          x, y = current_position
-          case current_direction
-          when '^'
-            if points.include?([x, y + 1])
-              temp_area[current_position] = '|'
-            else
-              temp_area[current_position] = "/"  if next_direction == '^'
-            end
-          when 'v'
-            if points.include?([x, y - 1])
-              temp_area[current_position] = '|'
-            else
-              temp_area[current_position] = "\\" if next_direction == 'v'
-            end
-          when '<'
-            if points.include?([x + 1, y])
-              temp_area[current_position] = '-'
-            else
-              temp_area[current_position] = "\\" if next_direction == '<'
-            end
-          when '>'
-            if points.include?([x - 1, y])
-              temp_area[current_position] = '-'
-            else
-              temp_area[current_position] = "/"  if next_direction == '>'
-            end
+          skip_points << new_point
+
+          @intersection_counts.delete(point)
+          points.delete(point)
+          points.delete(new_point)
+          if cars.keys.include?(new_point)
+            @intersection_counts.delete(new_point)
+            cars.delete(new_point)
           end
-        end
-        temp_area[next_position] = next_direction
-
-        new_cars << [next_position, next_direction]
-      end
-
-      cars = new_cars
-      area = temp_area
-      
-      # crash
-      cars.map(&:first).each_with_index do |e, index|
-        crash << index if next_positions.count(e) != 1
-      end
-      if crash.size != 0
-        ca = []
-        cars.each_with_index { |e, index| ca << e unless crash.include? index }
-
-        if status
-          puts 'Part 1: ' + cars[crash[0]][0].join(',')
-          status = false
-        end
-
-        crash.map { |n| cars[n][0] }.uniq.each do |point|
-          area[point] = origin[point]
-        end
-
-        if ca.size == 1
-          puts 'Part 2: ' + ca[0][0].join(',')
-          break
-        end
-
-        cars = ca
-      end
-
-      # map
-      if cars.include?([[39, 110], nil])
-        p_area area
-        binding.pry
-      end
-
-    end
-  end
-
-  def p_area(area)
-    points = area.keys
-    max_x = points.map(&:first).max
-    max_y = points.map(&:last).max
-    file  = 'map'
-
-    (0..max_y).each do |y|
-      line = ''
-      (0..max_x).each do |x|
-        if points.include? [x, y]
-          line += area[[x, y]] || 'X'
         else
-          line += ' '
+          new_direction = car_next_direction(point, new_point, direction)
+          cars[new_point] = new_direction
+
+          points.append(new_point)
         end
+
+        points.delete(point)
       end
-      File.write(file, line + "\n", mode: 'a')
+
+      @cars = sort_cars(cars)
     end
+
+    # Part 2
+    puts @cars.keys.join(',')
   end
 
-  def next_intersection_direction(direction, turn)
-    case direction
-    when '^'
-      case turn
-      when :left
-        '<'
-      when :straight
-        '^'
-      when :right
-        '>'
-      end
-    when 'v'
-      case turn
-      when :left
-        '>'
-      when :straight
-        'v'
-      when :right
-        '<'
-      end
-    when '<'
-      case turn
-      when :left
-        'v'
-      when :straight
-        '<'
-      when :right
-        '^'
-      end
-    when '>'
-      case turn
-      when :left
-        '^'
-      when :straight
-        '>'
-      when :right
-        'v'
-      end
-    end
+  private
+
+  # sort from top to bottom, then left to right
+  def sort_cars(cars)
+    cars.to_a
+        .group_by { |e| e[0][1] }
+        .reduce({}) { |res, (k, v)| res.merge(k => v.sort_by { |e| e[0][0] }) }
+        .sort.to_h.values
+        .reduce([]) { |res, e| res + e }
+        .to_h
   end
 
-  def step_position(cars, map)
-    result = []
-    dh = {
-      '^' => [0, -1],
-      'v' => [0, 1],
-      '<' => [-1, 0],
-      '>' => [1, 0],
-    }
+  # next car point and direction
+  def car_next_point(point, direction)
+    x, y = point
 
-    cars.each_with_index do |(point, direction), index|
-      x, y = point
-      case map[point]
-      when "/"
-        result << [x + 1, y] if direction == '^'
-        result << [x - 1, y] if direction == 'v'
-      when "\\"
-        result << [x - 1, y] if direction == '^'
-        result << [x + 1, y] if direction == 'v'
-      else
-        dx, dy = dh[direction]
-        result << [x + dx, y + dy]
+    {
+      '<' => [x - 1, y],
+      '>' => [x + 1, y],
+      '^' => [x, y - 1],
+      'v' => [x, y + 1]
+    }[direction]
+  end
+
+  def car_next_direction(p, np, direction)
+    directions = %w[< ^ > v]
+    count = @intersection_counts[p]
+    state = @map[np]
+
+    @intersection_counts.delete(p)
+    @intersection_counts[np] = count if state != '+'
+    @intersection_counts[np] = count + 1 if state == '+'
+
+    case state
+    when '-', '|'
+      direction
+    when "\\"
+      @directions[-1 - @directions.index(direction)]
+    when '/'
+      @directions[@directions.index(direction) - 2]
+    when '+'
+      case count % 3
+      # turn left
+      when 0
+        directions[directions.index(direction) - 1]
+      # turn straignt
+      when 1
+        direction
+      # turn right
+      when 2
+        directions[directions.index(direction) - 3]
       end
     end
-    
-    result
-  rescue TypeError
-    binding.pry
   end
 end
 
-puts Handle.new.location_of_first_crash
-
+Handle.new.run
