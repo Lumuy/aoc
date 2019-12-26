3a47321b06cde0ccbe03e0923f959d12f06ab285
lumuy <dxgnaw@gmail.com>
2019-12-26 23:07:40 +0800

    un

--- 2019/day18/many_world_interpretation.go
+++ 2019/day18/many_world_interpretation.go
@@ -1,197 +1,208 @@
 package main
 
 import (
-	intcode "aoc/2019/intcode_program"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strings"
 )
 
-const allDoors string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const allKeys string = "abcdefghijklmnopqrstuvwxyz"
-
 type Point struct {
-	x, y int
+	X, Y int
 }
 
-type Unit struct {
-	point Point
-	name  string
-	steps int
+/*
+	Keys number is less than 32
+	so a 32 bit unsigned integer to resprent collected keys
+
+	1 ---> collected
+	0 ---> uncollected
+
+	n ----bits------ 31 30 29 .... 2 1 0
+
+	3 keys required  <===> 000...111 binary
+*/
+type Connection struct {
+	NextNode     *Node
+	RequiredKeys uint32
+	Distance     int
 }
 
-type Movement struct {
-	gotKeys     []string
-	curEntrance Point
-	curSteps    int
+type Node struct {
+	Key         byte
+	Position    Point
+	Connections []Connection
 }
 
-func getCurrentArea(in string, m Movement) map[Point]string {
-	area := make(map[Point]string)
-	in = strings.Trim(in, "\n")
-
-	openedDoors := make([]string, 0)
-	for _, key := range m.gotKeys {
-		openedDoors = append(openedDoors, strings.ToUpper(key))
-	}
-
-	y := 0
-	for _, line := range strings.Split(in, "\n") {
-		for x, c := range strings.Split(line, "") {
-			p := Point{x, y}
-			if intcode.SliceContains(m.gotKeys, c) ||
-				intcode.SliceContains(openedDoors, c) ||
-				c == "@" {
-				area[p] = "."
-			} else {
-				area[p] = c
-			}
-		}
-		y++
-	}
-	area[m.curEntrance] = "@"
-
-	return area
+type Item struct {
+	Position     Point
+	RequiredKeys uint32
+	Distance     int
 }
 
-func increaseMovement(in string, m Movement) ([]Movement, bool) {
-	ms := make([]Movement, 0)
-	area := getCurrentArea(in, m)
-
-	curReachableKeys := reachableKeys(area, m.curEntrance)
-	if len(curReachableKeys) == 0 {
-		return ms, false
-	}
-	// fmt.Println("----reachable keys------>", curReachableKeys)
-
-	for _, u := range curReachableKeys {
-		steps := m.curSteps + u.steps
-		keys := append(m.gotKeys, u.name)
-
-		ms = append(ms, Movement{keys, u.point, steps})
-	}
-
-	return ms, true
-}
-
-func getArea(input string) map[Point]string {
-	area := make(map[Point]string)
-	input = strings.Trim(input, "\n")
-
-	y := 0
-	for _, line := range strings.Split(input, "\n") {
-		for x, c := range strings.Split(line, "") {
-			area[Point{x, y}] = c
-		}
-		y++
-	}
-
-	return area
-}
-
-func pointsContains(points []Point, point Point) bool {
-	for _, p := range points {
-		if p == point {
-			return true
-		}
-	}
-	return false
-}
-
-func stepPoint(area map[Point]string, cur Point) ([]Point, map[Point]string) {
-	points := make([]Point, 0)
-	keys := make(map[Point]string)
-
-	for _, p := range []Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
-		np := Point{p.x + cur.x, p.y + cur.y}
-		if area[np] == "." {
-			points = append(points, np)
-		} else if strings.Contains(allKeys+allDoors, area[np]) {
-			keys[np] = area[np]
-		}
-	}
-
-	return points, keys
-}
-
-func reachableKeys(area map[Point]string, entrance Point) (keys []Unit) {
-	points := []Point{entrance}
-	walked := []Point{entrance}
-
-	for steps := 1; len(points) > 0; steps++ {
-		newPoints := make([]Point, 0)
-
-		for _, p := range points {
-			ps, ks := stepPoint(area, p)
-
-			for _, p := range ps {
-				if !pointsContains(walked, p) {
-					walked = append(walked, p)
-					newPoints = append(newPoints, p)
-				}
-			}
-			for p, k := range ks {
-				if strings.Contains(allKeys, k) {
-					keys = append(keys, Unit{p, k, steps})
-				}
-			}
-		}
-		points = newPoints
-	}
-
-	return keys
-}
-
-func getEntrance(area map[Point]string) (entrance Point) {
-	for p, c := range area {
-		if c == "@" {
-			entrance = p
-			break
-		}
-	}
-
-	return entrance
-}
-
-func shortestPathSteps(input string) (steps int) {
-	area := getArea(input)
-	entrance := getEntrance(area)
-	movements := []Movement{{[]string{}, entrance, 0}}
-
-	for {
-		goon := true
-		newMovements := make([]Movement, 0)
-
-		for _, m := range movements {
-			ms, done := increaseMovement(input, m)
-			newMovements = append(newMovements, ms...)
-			goon = goon && done
-		}
-
-		if !goon {
-			break
-		}
-
-		movements = newMovements
-
-		fmt.Println("-----increase movements size---collected keys number---> ", len(movements), len(movements[0].gotKeys))
-	}
-
-	for i, m := range movements {
-		if i == 0 || steps > m.curSteps {
-			steps = m.curSteps
-		}
-	}
-
-	return steps
-}
+type Area map[Point]byte
 
 func main() {
-	{
-		// Part 1
-		dat, _ := ioutil.ReadFile("input")
-		input := string(dat)
-		steps := shortestPathSteps(input)
-		fmt.Println(steps)
+	input := readFile("input")
+	// Part 1
+	n := shortestPathSteps(input)
+	fmt.Println(n)
+}
+
+func shortestPathSteps(input string) int {
+	area := parseInput(input)
+	return process(area)
+}
+
+func process(area Area) int {
+	keysNumber := 0
+	nodes := make(map[byte]*Node)
+
+	for p, char := range area {
+		if isKey(char) {
+			keysNumber++
+		}
+		if isEntrance(char) || isKey(char) {
+			nodes[char] = &Node{Key: char, Position: p}
+		}
 	}
+
+	for _, node := range nodes {
+		var spreads []Item
+		spreads = append(spreads, Item{node.Position, 0, 0})
+
+		seen := make(map[Point]bool)
+		seen[node.Position] = true
+
+		// Spread on up, down, left and right directions
+		for len(spreads) != 0 {
+			current := spreads[0]
+			spreads = spreads[1:]
+
+			for _, nextPos := range current.Position.Adjacents() {
+				if seen[nextPos] {
+					continue
+				}
+
+				seen[nextPos] = true
+				nextVal := area[nextPos]
+
+				if nextVal == '.' || isEntrance(nextVal) {
+					spreads = append(spreads, Item{nextPos, current.RequiredKeys, current.Distance + 1})
+				} else if isDoor(nextVal) {
+					spreads = append(spreads, Item{nextPos, current.RequiredKeys | bitFromDoor(nextVal), current.Distance + 1})
+				} else if isKey(nextVal) {
+					spreads = append(spreads, Item{nextPos, current.RequiredKeys | bitFromKey(nextVal), current.Distance + 1})
+
+					node.Connections = append(node.Connections, Connection{
+						NextNode:     nodes[nextVal],
+						RequiredKeys: current.RequiredKeys,
+						Distance:     current.Distance + 1,
+					})
+				}
+			}
+		}
+	}
+
+	// Find start positions
+	var entrance *Node
+	for _, node := range nodes {
+		if isEntrance(node.Key) {
+			entrance = node
+		}
+	}
+
+	var allKeys uint32 = (1 << keysNumber) - 1
+	cs := make([]string, 0)
+	return find(entrance, 0, allKeys, 0, math.MaxInt32, cs)
+}
+
+func find(node *Node, collectedKeys, allKeys uint32, curDistance, bestDistance int, cs []string) int {
+	if keysCollected(collectedKeys, allKeys) {
+		return curDistance
+	}
+
+	fmt.Println("collected keys------> ", cs)
+
+	for _, conn := range node.Connections {
+		if !keysCollected(collectedKeys, conn.RequiredKeys) {
+			continue
+		}
+
+		if keysCollected(collectedKeys, bitFromKey(conn.NextNode.Key)) {
+			continue
+		}
+
+		newDistance := curDistance + conn.Distance
+		if newDistance > bestDistance {
+			continue
+		}
+
+		nextCollectedKeys := collectedKeys | bitFromKey(conn.NextNode.Key)
+		newCs := append(cs, string(conn.NextNode.Key))
+		distance := find(conn.NextNode, nextCollectedKeys, allKeys, newDistance, bestDistance, newCs)
+		bestDistance = minInt(bestDistance, distance)
+	}
+
+	return bestDistance
+}
+
+func (p Point) Add(dp Point) Point {
+	return Point{p.X + dp.X, p.Y + dp.Y}
+}
+
+func (p Point) Adjacents() (r []Point) {
+	for _, dp := range []Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
+		r = append(r, p.Add(dp))
+	}
+	return r
+}
+
+func readFile(filename string) string {
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(string(dat))
+}
+
+func parseInput(input string) Area {
+	area := make(map[Point]byte)
+	for y, line := range strings.Split(input, "\n") {
+		for x, v := range []byte(line) {
+			area[Point{x, y}] = v
+		}
+	}
+	return area
+}
+
+func isEntrance(char byte) bool {
+	return char == '@'
+}
+
+func isKey(char byte) bool {
+	return char >= 'a' && char <= 'z'
+}
+
+func isDoor(char byte) bool {
+	return char >= 'A' && char <= 'Z'
+}
+
+func bitFromKey(key byte) uint32 {
+	return 1 << (key - 'a')
+}
+
+func bitFromDoor(door byte) uint32 {
+	return 1 << (door - 'A')
+}
+
+func keysCollected(collected, allKeys uint32) bool {
+	return collected&allKeys == allKeys
+}
+
+func minInt(l, r int) int {
+	if l < r {
+		return l
+	}
+	return r
 }
